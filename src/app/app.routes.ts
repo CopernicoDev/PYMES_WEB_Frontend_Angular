2091e7d0208fa45188d91c06ba52cc218c778815
Nicolás Giraldo Muñoz <giraldomunoznicolas900@gmail.com>
2026-01-26 20:19:13 +0000

    Conectando los diferentes componentes de la web

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Services } from './services/services';
 
 export const routes: Routes = [
-    
+	{ path: '', component: Home },
+	{ path: 'services', component: Services },
 ];
