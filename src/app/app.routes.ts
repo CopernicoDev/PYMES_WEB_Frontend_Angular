93ad05f3be32433a6130702f59e2a75bb0c29e18
Nicolás Giraldo Muñoz <giraldomunoznicolas900@gmail.com>
2026-01-26 20:45:00 +0000

    Linkeando los apartado de sign-up y login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Services } from './services/services';
+import { Login } from './login/login';
+import { SignUp } from './sign-up/sign-up';
 
 export const routes: Routes = [
 	{ path: '', component: Home },
 	{ path: 'services', component: Services },
+	{ path: 'login', component: Login },
+{ path: 'sign-up', component: SignUp }
 ];
