51aac3a07e02f3b4fbb80641a7595dfea9402e48
Nicolás Giraldo Muñoz <giraldomunoznicolas900@gmail.com>
2026-02-14 21:10:54 +0000

    Creando el componente contact

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Home } from './home/home';
 import { Services } from './services/services';
 import { Login } from './login/login';
 import { SignUp } from './sign-up/sign-up';
+import { Contact } from './contact/contact';
 
 export const routes: Routes = [
 	{ path: '', component: Home },
 	{ path: 'services', component: Services },
 	{ path: 'login', component: Login },
-{ path: 'sign-up', component: SignUp }
+{ path: 'sign-up', component: SignUp },
+{ path: 'contacto', component: Contact }
 ];
