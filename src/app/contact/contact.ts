2f231c6f6df3009a65072bc9f0f64eec8d13dd94
Nicolás Giraldo Muñoz <giraldomunoznicolas900@gmail.com>
2026-02-19 18:09:00 +0000

    Realizando las conexiones entre las páginas

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   templateUrl: './contact.html',
-  styleUrls: ['./contact.css']
+  styleUrls: ['./contact.css'],
+  imports: [RouterLink]
 })
 export class Contact {}
