4a3c67f52538d075f88df2c80a2444de7d7c3f4a
Nicolás Giraldo Muñoz <giraldomunoznicolas900@gmail.com>
2026-02-19 19:09:09 +0000

    Arreglando errores y puliendo css y maquetación

--- src/app/services/services.ts
+++ src/app/services/services.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-services',
   standalone: true,
   templateUrl: './services.html',
   styleUrls: ['./services.css'],
+  imports: [RouterLink],
 })
 export class Services {}
