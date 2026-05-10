f88a92d8106970f35aef112984f2a8a76a4361d0
Nicolás Giraldo Muñoz <giraldomunoznicolas900@gmail.com>
2026-05-10 22:08:36 +0100

    Creando componente reutilizable para los formularios

---
+++ src/app/components/input/input.ts
@@ -0,0 +1,20 @@
+import { Component, input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  imports: [ReactiveFormsModule],
+  templateUrl: './input.html',
+  styleUrl: './input.css',
+})
+export class Input {
+
+  label = input.required<string>();
+  id = input.required<string>();
+  type = input<string>('text');
+  placeholder = input<string>('');
+
+  control = input.required<FormControl>();
+
+
+}
--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { Input } from '../../components/input/input';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   templateUrl: './contact.html',
   styleUrls: ['./contact.css'],
-  imports: [RouterLink]
+  imports: [RouterLink, Input, ReactiveFormsModule]
 })
-export default class Contact { }
+export default class Contact {
+  contactForm = new FormGroup({
+    name: new FormControl(''),
+    mail: new FormControl(''),
+    subject: new FormControl(''),
+    message: new FormControl(''),
+  })
+}
