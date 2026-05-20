911d61d738c6e088cfb9400644a2f3b63aa403eb
Nicolás Giraldo Muñoz <giraldomunoznicolas900@gmail.com>
2026-05-20 15:22:39 +0100

    Modificando los formularios de los apartados login y sign-up

--- src/app/components/input/input.ts
+++ src/app/components/input/input.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core'; // 👈 Añadimos 'signal'
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
@@ -15,7 +15,15 @@ export class Input {
   placeholder = input<string>('');
   password = input<string>();
 
+  // 👈 1. Añadimos un input opcional para indicar qué icono usar (ej: 'email')
+  icon = input<string>();
+
   control = input.required<FormControl>();
 
+  // 👈 2. Señal para alternar la visibilidad de la contraseña
+  isPasswordVisible = signal(false);
 
+  togglePasswordVisibility() {
+    this.isPasswordVisible.update(visible => !visible);
+  }
 }
